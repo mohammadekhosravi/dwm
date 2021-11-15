473c96091fe9c1d2ce48dad7b9cbfb86fb35f75b
mohammadekhosravi <mohammadekhosravi@yahoo.com>
2021-11-15 16:38:34 +0330

    change TERMINAL to konsole and change modkey to Super

--- config.def.h
+++ config.def.h
@@ -44,7 +44,8 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+// Mod4Mask = Super key / Mod1Mask = Alt key
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -57,7 +58,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "konsole", "--hide-menubar" ,NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
