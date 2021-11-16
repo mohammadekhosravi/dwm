9291bd497f07cdb678107b6348aeb10690e5c6d8
mohammadekhosravi <mohammadekhosravi@yahoo.com>
2021-11-16 23:34:20 +0330

    Change border color

--- config.def.h
+++ config.def.h
@@ -14,12 +14,13 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_border[]        = "#dc2626";
 static const unsigned int baralpha = 0xdf;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_border  },
 };
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
