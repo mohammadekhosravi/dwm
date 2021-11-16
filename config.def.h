b646fae9fd115f05c9bb6a83f70e27d4745a5100
mohammadekhosravi <mohammadekhosravi@yahoo.com>
2021-11-16 23:17:59 +0330

    Small font size change

--- config.def.h
+++ config.def.h
@@ -7,8 +7,8 @@ static const unsigned int gappx[]   = { 4 };   /* default gap between windows in
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12" };
-static const char dmenufont[]       = "monospace:size=12";
+static const char *fonts[]          = { "monospace:size=10" };
+static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -100,7 +100,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "konsole", "--hide-menubar", NULL };
-static const char *webcmd[]  = { "google-chorme", NULL };
+static const char *webcmd[]  = { "google-chrome", NULL };
 #include "shift-tools.c"
 
 static Key keys[] = {
